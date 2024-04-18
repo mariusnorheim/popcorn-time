bed1e9824d919f856772319bc72e0548b0932706
Marius Norheim <marius@mariusnorheim.dev>
2024-04-18 22:33:48 +0200

    Adjusted card height

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ movie }) => (
     <div className="max-w-m rounded-md overflow-hidden shadow-lg shadow-slate-800/50 m-2 bg-gradient-to-r from-sky-900/80 to-cyan-700/90">
         <div className="h-100 border border-slate-800 overflow-hidden">
-            <Image className="w-full object-cover brightness-90 hover:brightness-105 hover:scale-105" src={movie.imageUrl} alt={movie.name} width="400" height="400" />
+            <Image className="w-full object-cover h-96 brightness-90 hover:brightness-105 hover:scale-105" src={movie.imageUrl} alt={movie.name} width="400" height="400" />
         </div>
         <div className="px-6 py-4">
             <h2 className="text-white font-bold text-2xl mb-2">{movie.name}</h2>
